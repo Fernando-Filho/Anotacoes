feb2bc43fe95c159bfc361a3428ca991b52b1ea8
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-05-17 22:41:51 -0300

    Conclusão da barra de pesquisa

--- anotacoes/src/components/Search/Index.js
+++ anotacoes/src/components/Search/Index.js
@@ -1,15 +1,29 @@
 import "./Search.css"
 
+import { useState } from "react";
+
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Search = (props) => {
+
+    const [searchTitle, setSearchTitle] = useState()
+
+    function handleSearchNote(props){
+
+        setSearchTitle(props.target.value)
+        console.log(props.target.value)
+    }
+
     return(
-        <div ClassName="Search">
+        <div className="Search">
             <input 
                 type="text"
-                placeholder="Busque aqui!!"
-                />
-                <AiOutlineSearch/>
+                value={searchTitle}
+                placeholder={props.placeholder}
+                onChange={handleSearchNote}
+
+            />
+            <AiOutlineSearch className="IconSearch" size={24} color="white"/>
         </div>
     )
 }
