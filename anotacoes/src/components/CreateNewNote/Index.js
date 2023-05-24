408b2b12936b2e130d482202745605e2110382c0
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-05-23 22:22:27 -0300

    Um novo container para melhor posicionar.

--- anotacoes/src/components/CreateNewNote/Index.js
+++ anotacoes/src/components/CreateNewNote/Index.js
@@ -32,36 +32,41 @@ const CreateNewNote = (props) => {
         return(
             <form className="CreateNewNote"  onSubmit={handleCreateNewNote}>
 
+                <div className="Modal">
 
-                <div className="Inputs">
+                    <h1>Crie sua anotação</h1>
 
-                    <input  type="text"
-                            value={title}
-                            placeholder="Título"
-                            onChange={e => setTitle(e.target.value)}
-                            required/>
+                    <div className="Inputs">
 
-                    <textarea   value={text}
-                                placeholder="Assunto..."
-                                onChange={e => setText(e.target.value)}
+                        <input  type="text"
+                                value={title}
+                                placeholder="Título"
+                                onChange={e => setTitle(e.target.value)}
                                 required/>
 
+                        <textarea   value={text}
+                                    placeholder="Assunto..."
+                                    onChange={e => setText(e.target.value)}
+                                    required/>
+
+                    </div>
+
+                    <div className="orientionButtons">
+
+                        <button type="submit"
+                                onClick={handleCreateNewNote}>
+                                    {/* <AiOutlineCheck 
+                                        size={25}
+                                        color="var(--Preto1)"/> */}
+
+                                    <span>SALVAR</span>
+                        </button>
+                        
+                        <button type="reset"
+                                onClick={handleExitCreateNewNote}
+                                >CANCELAR</button>
+                    </div>
                 </div>
-
-                <div className="orientionButtons">
-
-                    <button type="submit"
-                            onClick={handleCreateNewNote}>
-                                <AiOutlineCheck 
-                                    size={25}
-                                    color="var(--Preto1)"/>
-                    </button>
-                    
-                    <button type="reset"
-                            onClick={handleExitCreateNewNote}
-                            />
-                </div>
-
             </form>
         )
     }
