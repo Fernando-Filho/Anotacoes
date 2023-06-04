53948257bea8bfcc88bc73f162caa7a236ce84f6
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-06-04 10:55:16 -0300

    Mudança de CaixadeConteudo para Main

---
+++ anotacoes/src/components/Main/Index.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import { MainContainer, AddButton } from './style'
+
+import Conteudo from "../Conteudo/Index"
+import CreateNewNote from "../CreateNewNote/Index";
+
+const Main = ({list, handleSaveNote}) =>{
+
+    const [openCreateNewNote, setOpenCreateNewNote] = useState(false)
+
+    const ToAddButton = () => {
+        if(openCreateNewNote === false){
+          return(
+            <AddButton onClick={() => setOpenCreateNewNote(true)}>+</AddButton>
+          )
+        }
+    }
+
+    return(
+        <MainContainer>
+            {list.map((props, index) => (
+                <Conteudo key={index} title={props.title} text={props.text} />
+            ))}
+
+            <CreateNewNote  handleSaveNote={handleSaveNote}
+                            openCreateNewNote={openCreateNewNote}
+                            setOpenCreateNewNote={setOpenCreateNewNote}/>
+
+            <ToAddButton/>
+        </MainContainer>
+    )
+}
+
+export default Main
---
+++ anotacoes/src/components/Main/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 16rem;
+    min-width: 100vw;
+`
+
+export const AddButton = styled.button`
+    position: fixed;
+    display: flex;
+    bottom: 90px;
+    right: 25px;
+    align-items: center;
+    justify-content: center;
+    height: 60rem;
+    width: 60rem;
+    border-radius: 50%;
+    border: solid 1rem rgba(0, 0, 0, 0.1);
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.5);
+    font-size: 40rem;
+
+    &:hover{
+    transition: .5s;
+    box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.5);
+}
+`
